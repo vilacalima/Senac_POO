e543a1d13a6d8c896fc40cf40c43098a18f2bdd6
ROBSON VILACA LIMA <robson.vlima3@senacsp.edu.br>
2023-02-10 20:43:20 -0300

    Aula1

---
+++ src/Aula1/Aula1.java
@@ -0,0 +1,17 @@
+package Aula1;
+
+public class Aula1 {
+
+	//Classe principal
+	//Todo projeto tem uma unica classe main
+	public static void main(String[] args) {
+		
+		System.out.println("Aula 1");
+		
+		String a = "text";
+		a.charAt(1);
+
+		System.out.println(a.charAt(1));
+	}
+
+}
